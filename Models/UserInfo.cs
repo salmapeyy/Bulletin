69e51dde4b914586bda6f76a5bb86538156a51a4
Michilegaspino <116810402+Michilegaspino@users.noreply.github.com>
2023-07-07 20:32:17 +0800

    Update UserInfo.cs
    minor changes

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -11,4 +11,8 @@ namespace Models
 		public string Username { get; set; }
 		public string Password { get; set; }
 	}
+	public class AdminInfo
+	    public string FacultyNumber { get; set; }
+		public string PassAdmin {get; set; }
+	}
 }
